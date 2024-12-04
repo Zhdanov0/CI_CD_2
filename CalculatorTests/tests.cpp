86a7a380bb0495bbd9b8f4db777f6655c46b015a
Zhdanov0 <zhdanov228@gmail.com>
2024-12-05 02:26:09 +0300

    add tests project

---
+++ CalculatorTests/tests.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "../Calculator/calculator.cpp"
+
+Operations operations;
+
+TEST(CalculatorTests, Add) {
+    EXPECT_EQ(operations.add(7, 3), 10);
+    EXPECT_EQ(operations.add(-1, 1), 0);
+}
+
+TEST(CalculatorTests, Subtract) {
+    EXPECT_EQ(operations.subtract(12, 10), 2);
+    EXPECT_EQ(operations.subtract(-1, -1), 0);
+}
+
+TEST(CalculatorTests, Multiply) {
+    EXPECT_EQ(operations.multiply(5, 6), 30);
+    EXPECT_EQ(operations.multiply(-8, 8), -64);
+}
+
+TEST(CalculatorTests, Divide) {
+    EXPECT_EQ(operations.divide(18, 3), 6);
+    EXPECT_EQ(operations.divide(5, 0), 0);
+}
