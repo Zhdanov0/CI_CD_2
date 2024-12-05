f3e44c8c1cec6d557cda2b8779e2ca2c2c3bb3f9
Zhdanov0 <zhdanov228@gmail.com>
2024-12-05 15:03:58 +0300

    add doxyfile

--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -1,17 +1,49 @@
 #pragma once
 
+/**
+ * @class Operations
+ * @brief Класс для выполнения базовых арифметических операций.
+ */
 class Operations {
 
 public:
+    /**
+     * @brief Сложение двух чисел.
+     * @param a Первое число.
+     * @param b Второе число.
+     * @return Сумма чисел.
+     */
+    double add(double a, double b);
 
-	double add(double a, double b);
+    /**
+     * @brief Вычитание двух чисел.
+     * @param a Первое число.
+     * @param b Второе число.
+     * @return Разность чисел.
+     */
+    double subtract(double a, double b);
 
-	double subtract(double a, double b);
+    /**
+     * @brief Умножение двух чисел.
+     * @param a Первое число.
+     * @param b Второе число.
+     * @return Произведение чисел.
+     */
+    double multiply(double a, double b);
 
-	double multiply(double a, double b);
+    /**
+     * @brief Деление двух чисел.
+     * @param a Первое число.
+     * @param b Второе число.
+     * @return Результат деления или 0, если деление на 0.
+     */
+    double divide(double a, double b);
 
-	double divide(double a, double b);
-
-	double power(double a, int b);
-
+    /**
+     * @brief Возведение числа в степень.
+     * @param a Основание.
+     * @param b Показатель степени.
+     * @return Результат возведения в степень.
+     */
+    double power(double a, int b);
 };
